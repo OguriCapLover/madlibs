e5b3ba6923be066410fcf2f361a8b8c890657012
OguriCapLover <joseph116521@mcpsva.org>
2025-10-20 10:03:45 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,39 @@
+"""
+Filename: madlibs.py
+Author: Kilbourne Joseph
+Created: 8/26/2025
+Instructor: Holtslander
+"""
+
+Color = input("Give me a color\n")
+Gear = input("What is a type of clothing or gear worn during exercise\n")
+Verb = input("Give me a verb ending in -ing\n")
+Location = input(f"A location where race competitions would happen\n")
+Emotion = input("Give me a feeling or emotion")
+Snack = input("Name a type of snack or energy food\n")
+Nickname = input("A nickname for a speedy horse?\n")
+Instrument = input("Give me a musical instrument or sound\n")
+Season = input("Name a season\n")
+Plant = input("Name a type of flower or plant\n")
+Adjective = input("Give me an adjective meaning fast or quick\n")
+Number = input("Give me a number between 1-10\n")
+Shoe = input("Name a type of footwear used for running\n")
+Friend = input("Give me another word for friend\n")
+PracticeVerb = input("Give me a verb related to practicing\n")
+Vehicle = input("Give me a type of vehicle for transportation\n")
+Nature = input("Nature feature like a hill\n")
+Encouragement = input("An encouraging phrase\n")
+Pride = input("A word that describes achievement\n")
+Dance = input("A type of dance\n")
+Date = input("Something used to see the date\n")
+
+print(f"In a world where Umas were known for their speed and spirit, a {Color}-clad athlete laced up her {Shoe} at the\n "
+      f"{Location}, ready for her next big race. The excitement of {Verb} filled the air, as trainers and her {Friend}\n "
+      f"gathered to watch. Her heart raced with {Emotion}, fueled by a quick bite of {Snack} and the cheers of fans\n "
+      f"calling her the {Nickname} of the track. Nearby, {Plant} swayed gently in the breeze, a quiet reminder of the\n "
+      f"{Season} season's promise. The {Instrument} played softly, blending with the sounds of boots pounding against\n "
+      f"the turf in {Adjective} strides. She had trained {Number} weeks for this moment, every step, every race,\n"
+      f" counting down the days on her {Date}. Sliding into her favorite{Gear}, she glanced toward the {Nature} beyond the track.\n"
+      f" Her {Vehicle} waited in the parking lot to take her home after the race. The crowd erupted in cheers, shouting,\n"
+      f" {Encouragement} as she surged forward. After crossing the finish line,\n "
+      f"she celebrated with a joyful {Dance} a perfect expression of the {Pride} only a true champion feels.\n")
